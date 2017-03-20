83e15d889daf36791663853af3ff548838de7f5b
Nicolas Chapon <nchapon@gmail.com>
2017-03-20 16:40:31 +0100

    Tables and metadata

---
+++ day2/util.lua
@@ -0,0 +1,18 @@
+function print_table(t)
+
+   for k,v in pairs(t) do
+      print(k.. ": " ..v)
+   end
+end
+
+
+function table_to_string(t)
+   local result = {}
+   for k,v in pairs(t) do
+      result[#result +1] = k.. ": " ..v
+   end
+   return table.concat(result, "\n")
+end
+
+
+-- setmetatable(greek_numbers, mt)
